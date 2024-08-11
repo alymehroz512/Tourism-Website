a26cfe83c7687ae7d02691d4dfe9a6ebc1d3c1b8
alymehroz <119129110+alymehroz512@users.noreply.github.com>
2024-08-11 16:17:44 +0500

    Responsive Toursim Website

---
+++ TREKTOVEWORLD/JS/loader.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate a delay for demonstration purposes (2 seconds)
+    setTimeout(function () {
+      // Hide the pre-loader
+      document.getElementById("preloader").style.display = "none";
+      
+      // Show the website content
+      document.getElementById("content").style.display = "block";
+    }, 2000);
+  });
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
